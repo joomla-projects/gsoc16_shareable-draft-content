1fe8f94b74f45f6343243a53d01a8eb4fee3ba06
Nikita Dhiman <nikitadhmn@gmail.com>
2016-06-15 13:31:52 +0530

    Update 3.7.0-2016-06-14.sql

--- administrator/components/com_admin/sql/updates/mysql/3.7.0-2016-06-14.sql
+++ administrator/components/com_admin/sql/updates/mysql/3.7.0-2016-06-14.sql
@@ -1,5 +1,5 @@
 CREATE TABLE if NOT EXISTS  `#__share_draft`(
-  `id` int(10) NOT NULL AUTO_INCREMENT COMMENT 'Primary Key to #__content',
+  `id` int(10) NOT NULL AUTO_INCREMENT COMMENT 'Primary Key',
   `title` varchar(255) NOT NULL,
   `created` datetime NOT NULL,
   `modified` datetime NOT NULL, 
