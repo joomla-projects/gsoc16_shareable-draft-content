d48ae61783af81db39bf61d06efee3522b71526d
nikitadhiman <nikitadhmn@gmail.com>
2016-07-08 01:03:45 +0530

    edit.php changes

--- administrator/components/com_content/views/article/tmpl/edit.php
+++ administrator/components/com_content/views/article/tmpl/edit.php
@@ -159,10 +159,9 @@ JFactory::getDocument()->addScriptDeclaration('
 		<input type="hidden" name="return" value="<?php echo $input->getCmd('return'); ?>" />
 		<?php echo JHtml::_('form.token'); ?>
 
-		<?php echo JLayoutHelper::render('joomla.content.global', $this); ?>
+		<?php echo JLayoutHelper::render('joomla.content.global', $this);
 		echo '<button type="button" class="btn btn-small" id="sharebutton">
 			<span>' . JText::_('COM_CONTENT_SHARE') . '</span>
-		</button>';
-		</div>
+		</button>';?>
 
 </form>
