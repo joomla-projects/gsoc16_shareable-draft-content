e2bb451d16a5ad8759d931eaa752f4f6af426a99
nikitadhiman <nikitadhmn@gmail.com>
2016-07-12 20:14:33 +0530

    Draft manager view part. To be updated more...

---
+++ administrator/components/com_content/views/shared/tmpl/default.php
@@ -0,0 +1,148 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_content
+ *
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+JHtml::addIncludePath(JPATH_COMPONENT . '/helpers/html');
+
+JHtml::_('bootstrap.tooltip');
+JHtml::_('behavior.multiselect');
+JHtml::_('formbehavior.chosen', 'select');
+
+$app       = JFactory::getApplication();
+$user      = JFactory::getUser();
+$userId    = $user->get('id');
+$listOrder = $this->escape($this->state->get('list.ordering'));
+$listDirn  = $this->escape($this->state->get('list.direction'));
+$archived  = $this->state->get('filter.published') == 2 ? true : false;
+$trashed   = $this->state->get('filter.published') == -2 ? true : false;
+$saveOrder = $listOrder == 'a.ordering';
+$columns   = 10;
+
+$assoc = JLanguageAssociations::isEnabled();
+?>
+
+<form action="<?php echo JRoute::_('index.php?option=com_content&view=shared'); ?>" method="post" name="adminForm" id="adminForm">
+    <?php if (!empty( $this->sidebar)) : ?>
+    <div id="j-sidebar-container" class="span2">
+        <?php echo $this->sidebar; ?>
+    </div>
+    <div id="j-main-container" class="span10">
+        <?php else : ?>
+        <div id="j-main-container">
+            <?php endif;?>
+            <?php
+            // Search tools bar
+            echo JLayoutHelper::render('joomla.searchtools.default', array('view' => $this));
+            ?>
+
+            <?php if (empty($this->items)) : ?>
+                <div class="alert alert-no-items">
+                    <?php echo JText::_('JGLOBAL_NO_MATCHING_RESULTS'); ?>
+                </div>
+            <?php else : ?>
+            <table class="table table-striped" id="articleList">
+                <thead>
+                <tr>
+                    <th width="1%" class="center">
+                        <?php echo JHtml::_('grid.checkall'); ?>
+                    </th>
+                    <th width="1%" class="nowrap hidden-phone">
+                        <?php echo JHtml::_('searchtools.sort', 'JGRID_HEADING_ID', 'a.id', $listDirn, $listOrder); ?>
+                    </th>
+                    <th>
+                        <?php echo JHtml::_('searchtools.sort', 'JGLOBAL_TITLE', 'a.title', $listDirn, $listOrder); ?>
+                    </th>
+                    <th width="10%" class="nowrap hidden-phone">
+                        <?php echo JHtml::_('searchtools.sort', 'JAUTHOR', 'a.created_by', $listDirn, $listOrder); ?>
+                    </th>
+                    <th width="10%" class="nowrap hidden-phone">
+                        <?php echo JHtml::_('searchtools.sort', 'JDATE', 'a.created', $listDirn, $listOrder); ?>
+                    </th>
+                    <th width="1%" class="nowrap hidden-phone">
+                        <?php echo JHtml::_('searchtools.sort', 'JSHARED_LINK', 'a.share', $listDirn, $listOrder); ?>
+                    </th>
+                </tr>
+                </thead>
+                <tfoot>
+                <tr>
+                    <td colspan="<?php echo $columns; ?>">
+                    </td>
+                </tr>
+                </tfoot>
+
+
+            <?php echo JHtml::_('form.token'); ?>
+        </div>
+</form>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
