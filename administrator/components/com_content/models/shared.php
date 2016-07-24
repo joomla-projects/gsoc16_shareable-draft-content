2cbc124e0906dcc33bc8150eaf5bf1029718549a
nikitadhiman <nikitadhmn@gmail.com>
2016-07-24 19:03:03 +0530

    controller-model update

--- administrator/components/com_content/models/shared.php
+++ administrator/components/com_content/models/shared.php
@@ -1,139 +1,394 @@
 <?php
-/**
- * @package     Joomla.Administrator
- * @subpackage  com_content
- *
- * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
- */
-
-defined('_JEXEC') or die;
-
-/**
- * Articles list controller class.
- *
- * @since  1.6
- */
-class ContentControllerShared extends JControllerAdmin
-{
 	/**
-	 * Constructor.
+	 * @package     Joomla.Administrator
+	 * @subpackage  com_content
 	 *
-	 * @param   array  $config  An optional associative array of configuration settings.
-	 *
-	 * @see     JController
-	 * @since   1.6
+	 * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+	 * @license     GNU General Public License version 2 or later; see LICENSE.txt
 	 */
-	public function __construct($config = array())
-	{
-		parent::__construct($config);
 
-		// Articles default form can come from the articles or featured view.
-		// Adjust the redirect view on the value of 'view' in the request.
-		if ($this->input->get('view') == 'featured')
+	defined('_JEXEC') or die;
+
+	/**
+	 * Methods supporting a list of shared article records.
+	 *
+	 * @since  3.7
+	 */
+	class ContentModelShared extends JModelList
+	{
+		/**
+		 * Constructor.
+		 *
+		 * @param   array  $config  An optional associative array of configuration settings.
+		 *
+		 * @since   1.6
+		 * @see     JController
+		 */
+		public function __construct($config = array())
 		{
-			$this->view_list = 'featured';
+			if (empty($config['filter_fields']))
+			{
+				$config['filter_fields'] = array(
+					'id', 'a.id',
+					'title', 'a.title',
+					'alias', 'a.alias',
+					'checked_out', 'a.checked_out',
+					'checked_out_time', 'a.checked_out_time',
+					'catid', 'a.catid', 'category_title',
+					'state', 'a.state',
+					'access', 'a.access', 'access_level',
+					'created', 'a.created',
+					'created_by', 'a.created_by',
+					'created_by_alias', 'a.created_by_alias',
+					'ordering', 'a.ordering',
+					'featured', 'a.featured',
+					'language', 'a.language',
+					'hits', 'a.hits',
+					'publish_up', 'a.publish_up',
+					'publish_down', 'a.publish_down',
+					'published', 'a.published',
+					'share', 'a.share',
+					'author_id',
+					'category_id',
+					'level',
+					'tag'
+				);
+
+				if (JLanguageAssociations::isEnabled())
+				{
+					$config['filter_fields'][] = 'association';
+				}
+			}
+
+			parent::__construct($config);
 		}
 
-		$this->registerTask('unfeatured', 'featured');
-	}
-
-	/**
-	 * Method to toggle the featured setting of a list of articles.
-	 *
-	 * @return  void
-	 *
-	 * @since   1.6
-	 */
-	public function featured()
-	{
-		// Check for request forgeries
-		JSession::checkToken() or jexit(JText::_('JINVALID_TOKEN'));
-
-		$user   = JFactory::getUser();
-		$ids    = $this->input->get('cid', array(), 'array');
-		$values = array('featured' => 1, 'unfeatured' => 0);
-		$task   = $this->getTask();
-		$value  = JArrayHelper::getValue($values, $task, 0, 'int');
-
-		// Access checks.
-		foreach ($ids as $i => $id)
+		/**
+		 * Method to auto-populate the model state.
+		 *
+		 * Note. Calling getState in this method will result in recursion.
+		 *
+		 * @param   string  $ordering   An optional ordering field.
+		 * @param   string  $direction  An optional direction (asc|desc).
+		 *
+		 * @return  void
+		 *
+		 * @since   1.6
+		 */
+		protected function populateState($ordering = 'a.id', $direction = 'desc')
 		{
-			if (!$user->authorise('core.edit.state', 'com_content.article.' . (int) $id))
+			$app = JFactory::getApplication();
+
+			$forcedLanguage = $app->input->get('forcedLanguage', '', 'cmd');
+
+			// Adjust the context to support modal layouts.
+			if ($layout = $app->input->get('layout'))
 			{
-				// Prune items that you can't change.
-				unset($ids[$i]);
-				JError::raiseNotice(403, JText::_('JLIB_APPLICATION_ERROR_EDITSTATE_NOT_PERMITTED'));
+				$this->context .= '.' . $layout;
+			}
+
+			// Adjust the context to support forced languages.
+			if ($forcedLanguage)
+			{
+				$this->context .= '.' . $forcedLanguage;
+			}
+
+			$search = $this->getUserStateFromRequest($this->context . '.filter.search', 'filter_search');
+			$this->setState('filter.search', $search);
+
+			$access = $this->getUserStateFromRequest($this->context . '.filter.access', 'filter_access');
+			$this->setState('filter.access', $access);
+
+			$authorId = $app->getUserStateFromRequest($this->context . '.filter.author_id', 'filter_author_id');
+			$this->setState('filter.author_id', $authorId);
+
+			$published = $this->getUserStateFromRequest($this->context . '.filter.published', 'filter_published', '');
+			$this->setState('filter.published', $published);
+
+			$categoryId = $this->getUserStateFromRequest($this->context . '.filter.category_id', 'filter_category_id');
+			$this->setState('filter.category_id', $categoryId);
+
+			$level = $this->getUserStateFromRequest($this->context . '.filter.level', 'filter_level');
+			$this->setState('filter.level', $level);
+
+			$language = $this->getUserStateFromRequest($this->context . '.filter.language', 'filter_language', '');
+			$this->setState('filter.language', $language);
+
+			$tag = $this->getUserStateFromRequest($this->context . '.filter.tag', 'filter_tag', '');
+			$this->setState('filter.tag', $tag);
+
+			// List state information.
+			parent::populateState($ordering, $direction);
+
+			// Force a language
+			if (!empty($forcedLanguage))
+			{
+				$this->setState('filter.language', $forcedLanguage);
+				$this->setState('filter.forcedLanguage', $forcedLanguage);
 			}
 		}
 
-		if (empty($ids))
+		/**
+		 * Method to get a store id based on model configuration state.
+		 *
+		 * This is necessary because the model is used by the component and
+		 * different modules that might need different sets of data or different
+		 * ordering requirements.
+		 *
+		 * @param   string  $id  A prefix for the store id.
+		 *
+		 * @return  string  A store id.
+		 *
+		 * @since   1.6
+		 */
+		protected function getStoreId($id = '')
 		{
-			JError::raiseWarning(500, JText::_('JERROR_NO_ITEMS_SELECTED'));
-		}
-		else
-		{
-			// Get the model.
-			$model = $this->getModel();
+			// Compile the store id.
+			$id .= ':' . $this->getState('filter.search');
+			$id .= ':' . $this->getState('filter.access');
+			$id .= ':' . $this->getState('filter.published');
+			$id .= ':' . $this->getState('filter.category_id');
+			$id .= ':' . $this->getState('filter.author_id');
+			$id .= ':' . $this->getState('filter.language');
 
-			// Publish the items.
-			if (!$model->featured($ids, $value))
+			return parent::getStoreId($id);
+		}
+
+		/**
+		 * Build an SQL query to load the list data.
+		 *
+		 * @return  JDatabaseQuery
+		 *
+		 * @since   1.6
+		 */
+		protected function getListQuery()
+		{
+			// Create a new query object.
+			$db = $this->getDbo();
+			$query = $db->getQuery(true);
+			$user = JFactory::getUser();
+			$app = JFactory::getApplication();
+
+			// Select the required fields from the table.
+			$query->select(
+				$this->getState(
+					'list.select',
+					'a.id, a.title, a.alias, a.checked_out, a.checked_out_time, a.catid' .
+					', a.state, a.access, a.created, a.created_by, a.created_by_alias, a.ordering, a.featured, a.language, a.hits' .
+					', a.publish_up, a.publish_down, a.share'
+				)
+			);
+			$query->from('#__content AS a');
+
+			// Join over the language
+			$query->select('l.title AS language_title, l.image AS language_image')
+				->join('LEFT', $db->quoteName('#__languages') . ' AS l ON l.lang_code = a.language');
+
+			// Join over the users for the checked out user.
+			$query->select('uc.name AS editor')
+				->join('LEFT', '#__users AS uc ON uc.id=a.checked_out');
+
+			// Join over the asset groups.
+			$query->select('ag.title AS access_level')
+				->join('LEFT', '#__viewlevels AS ag ON ag.id = a.access');
+
+			// Join over the categories.
+			$query->select('c.title AS category_title')
+				->join('LEFT', '#__categories AS c ON c.id = a.catid');
+
+			// Join over the users for the author.
+			$query->select('ua.name AS author_name')
+				->join('LEFT', '#__users AS ua ON ua.id = a.created_by');
+
+			// Join over the associations.
+			if (JLanguageAssociations::isEnabled())
 			{
-				JError::raiseWarning(500, $model->getError());
+				$query->select('COUNT(asso2.id)>1 as association')
+					->join('LEFT', '#__associations AS asso ON asso.id = a.id AND asso.context=' . $db->quote('com_content.item'))
+					->join('LEFT', '#__associations AS asso2 ON asso2.key = asso.key')
+					->group('a.id, l.title, l.image, uc.name, ag.title, c.title, ua.name');
 			}
 
-			if ($value == 1)
+			// Filter by access level.
+			if ($access = $this->getState('filter.access'))
 			{
-				$message = JText::plural('COM_CONTENT_N_ITEMS_FEATURED', count($ids));
+				$query->where('a.access = ' . (int) $access);
 			}
-			else
+
+			// Implement View Level Access
+			if (!$user->authorise('core.admin'))
 			{
-				$message = JText::plural('COM_CONTENT_N_ITEMS_UNFEATURED', count($ids));
+				$groups = implode(',', $user->getAuthorisedViewLevels());
+				$query->where('a.access IN (' . $groups . ')');
 			}
+
+			// Filter by published state
+			$published = $this->getState('filter.published');
+
+			if (is_numeric($published))
+			{
+				$query->where('a.state = ' . (int) $published);
+			}
+			elseif ($published === '')
+			{
+				$query->where('(a.state = 0 OR a.state = 1)');
+			}
+
+			// Filter by a single or group of categories.
+			$baselevel = 1;
+			$categoryId = $this->getState('filter.category_id');
+
+			if (is_numeric($categoryId))
+			{
+				$cat_tbl = JTable::getInstance('Category', 'JTable');
+				$cat_tbl->load($categoryId);
+				$rgt = $cat_tbl->rgt;
+				$lft = $cat_tbl->lft;
+				$baselevel = (int) $cat_tbl->level;
+				$query->where('c.lft >= ' . (int) $lft)
+					->where('c.rgt <= ' . (int) $rgt);
+			}
+			elseif (is_array($categoryId))
+			{
+				JArrayHelper::toInteger($categoryId);
+				$categoryId = implode(',', $categoryId);
+				$query->where('a.catid IN (' . $categoryId . ')');
+			}
+
+			// Filter on the level.
+			if ($level = $this->getState('filter.level'))
+			{
+				$query->where('c.level <= ' . ((int) $level + (int) $baselevel - 1));
+			}
+
+			// Filter by author
+			$authorId = $this->getState('filter.author_id');
+
+			if (is_numeric($authorId))
+			{
+				$type = $this->getState('filter.author_id.include', true) ? '= ' : '<>';
+				$query->where('a.created_by ' . $type . (int) $authorId);
+			}
+
+			// Filter by search in title.
+			$search = $this->getState('filter.search');
+
+			if (!empty($search))
+			{
+				if (stripos($search, 'id:') === 0)
+				{
+					$query->where('a.id = ' . (int) substr($search, 3));
+				}
+				elseif (stripos($search, 'author:') === 0)
+				{
+					$search = $db->quote('%' . $db->escape(substr($search, 7), true) . '%');
+					$query->where('(ua.name LIKE ' . $search . ' OR ua.username LIKE ' . $search . ')');
+				}
+				else
+				{
+					$search = $db->quote('%' . str_replace(' ', '%', $db->escape(trim($search), true) . '%'));
+					$query->where('(a.title LIKE ' . $search . ' OR a.alias LIKE ' . $search . ')');
+				}
+			}
+
+			// Filter on the language.
+			if ($language = $this->getState('filter.language'))
+			{
+				$query->where('a.language = ' . $db->quote($language));
+			}
+
+			// Filter by a single tag.
+			$tagId = $this->getState('filter.tag');
+
+			if (is_numeric($tagId))
+			{
+				$query->where($db->quoteName('tagmap.tag_id') . ' = ' . (int) $tagId)
+					->join(
+						'LEFT', $db->quoteName('#__contentitem_tag_map', 'tagmap')
+						. ' ON ' . $db->quoteName('tagmap.content_item_id') . ' = ' . $db->quoteName('a.id')
+						. ' AND ' . $db->quoteName('tagmap.type_alias') . ' = ' . $db->quote('com_content.article')
+					);
+			}
+
+			// Add the list ordering clause.
+			$orderCol = $this->state->get('list.ordering', 'a.id');
+			$orderDirn = $this->state->get('list.direction', 'desc');
+
+			if ($orderCol == 'a.ordering' || $orderCol == 'category_title')
+			{
+				$orderCol = 'c.title ' . $orderDirn . ', a.ordering';
+			}
+
+			// SQL server change
+			if ($orderCol == 'language')
+			{
+				$orderCol = 'l.title';
+			}
+
+			if ($orderCol == 'access_level')
+			{
+				$orderCol = 'ag.title';
+			}
+
+			$query->order($db->escape($orderCol . ' ' . $orderDirn));
+
+			return $query;
 		}
 
-		$view = $this->input->get('view', '');
-
-		if ($view == 'featured')
+		/**
+		 * Build a list of authors
+		 *
+		 * @return  JDatabaseQuery
+		 *
+		 * @since   1.6
+		 */
+		public function getAuthors()
 		{
-			$this->setRedirect(JRoute::_('index.php?option=com_content&view=featured', false), $message);
+			// Create a new query object.
+			$db = $this->getDbo();
+			$query = $db->getQuery(true);
+
+			// Construct the query
+			$query->select('u.id AS value, u.name AS text')
+				->from('#__users AS u')
+				->join('INNER', '#__content AS c ON c.created_by = u.id')
+				->group('u.id, u.name')
+				->order('u.name');
+
+			// Setup the query
+			$db->setQuery($query);
+
+			// Return the result
+			return $db->loadObjectList();
 		}
-		else
+
+		/**
+		 * Method to get a list of articles.
+		 * Overridden to add a check for access levels.
+		 *
+		 * @return  mixed  An array of data items on success, false on failure.
+		 *
+		 * @since   1.6.1
+		 */
+		public function getItems()
 		{
-			$this->setRedirect(JRoute::_('index.php?option=com_content&view=shared', false), $message);
+			$items = parent::getItems();
+
+			if (JFactory::getApplication()->isSite())
+			{
+				$user = JFactory::getUser();
+				$groups = $user->getAuthorisedViewLevels();
+
+				for ($x = 0, $count = count($items); $x < $count; $x++)
+				{
+					// Check the access level. Remove articles the user shouldn't see
+					if (!in_array($items[$x]->access, $groups))
+					{
+						unset($items[$x]);
+					}
+				}
+			}
+
+			return $items;
 		}
 	}
-
-	/**
-	 * Proxy for getModel.
-	 *
-	 * @param   string  $name    The model name. Optional.
-	 * @param   string  $prefix  The class prefix. Optional.
-	 * @param   array   $config  The array of possible config values. Optional.
-	 *
-	 * @return  JModel
-	 *
-	 * @since   1.6
-	 */
-	public function getModel($name = 'Shared', $prefix = 'ContentModel', $config = array('ignore_request' => true))
-	{
-		$model = parent::getModel($name, $prefix, $config);
-
-		return $model;
-	}
-
-	/**
-	 * Function that allows child controller access to model data
-	 * after the item has been deleted.
-	 *
-	 * @param   JModelLegacy  $model  The data model object.
-	 * @param   integer       $ids    The array of ids for items being deleted.
-	 *
-	 * @return  void
-	 *
-	 * @since   12.2
-	 */
-	protected function postDeleteHook(JModelLegacy $model, $ids = null)
-	{
-	}
-}
