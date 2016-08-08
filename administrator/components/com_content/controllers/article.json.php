5c7f85769baf42220fedaa998e217fa77c2d25bc
Nikita Dhiman <nikitadhmn@gmail.com>
2016-08-08 15:38:42 +0530

    minor

--- administrator/components/com_content/controllers/article.json.php
+++ administrator/components/com_content/controllers/article.json.php
@@ -1,4 +1,8 @@
 <?php
-//This is the article.json.php
-
-?>
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_content
+ *
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
