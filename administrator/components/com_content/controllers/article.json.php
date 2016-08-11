466b0068abd7e6a63d96fd2dc0ce3eab80178d75
Nikita Dhiman <nikitadhmn@gmail.com>
2016-08-11 11:08:51 +0530

    minor

--- administrator/components/com_content/controllers/article.json.php
+++ administrator/components/com_content/controllers/article.json.php
@@ -6,6 +6,15 @@
  * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
+ 
+ defined('_JEXEC') or die;
+/**
+ * The article controller
+ *
+ * @package     Joomla.Administrator
+ * @subpackage  com_content
+ * @since       1.6
+ */
  class ContentControllerArticle extends JControllerForm
 {
 	    /**
@@ -17,7 +26,7 @@
 	     */
 	     public function shareDraft()
 	     {
-		          $app    = JFactory::getApplication();
+	      	      $app    = JFactory::getApplication();
 		
 	    	      if (!JSession::checkToken())
 	    	      {
@@ -43,4 +52,5 @@
 		
 		          echo new JResponseJson($return, $message, $error);
 		          $app->close();
-	}
+	     }
+}
