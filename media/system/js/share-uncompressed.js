cb6afc3984099175f09f56e3554d512ca8d1e004
Nikita Dhiman <nikitadhmn@gmail.com>
2016-08-15 17:59:01 +0530

    js fixing...testing

--- media/system/js/share-uncompressed.js
+++ media/system/js/share-uncompressed.js
@@ -7,40 +7,39 @@
 /**
  * Calls the sending process of the config class
  */
-(function($){
-    $(document).ready(function ($){
-        $('#sharebutton').click(function ()
-        {
-            var share_draft = {
-                articleId : $('#jform_id').val(),
-                title     : $('#jform_title').val(),
+(function ($) {
+	$(document).ready(function ($) {
+		Joomla.submitbutton = function (task) {
+			if (task == 'shareDraft') {
+				var share_draft = {
+					articleId: $('#jform_id').val(),
+					title: $('#jform_title').val()
+				};
 
-            };
+				// Remove js messages, if they exist.
+				Joomla.removeMessages();
 
-            // Remove js messages, if they exist.
-            Joomla.removeMessages();
-
-            $.ajax({
-                    url: 'index.php?option=com_content&task=article.shareDraft',
-                    data: share_draft,
-                    dataType: "json"
-                })
-                .done(function (response) {
-                    // Render messages, if any.
-                    if (typeof response.messages == 'object' && response.messages !== null)
-                    {
-                        Joomla.renderMessages(response.messages);
-
-                        window.scrollTo(0, 0);
-                    }
-                })
-                    .fail(function(xhr, ajaxOptions, thrownError) {
-
-                        Joomla.renderMessages(xhr.responseText);
-
-                        window.scrollTo(0, 0);
-                    });
-
-                });
-        });
-    }(jQuery));
+                return false;
+				
+				$.ajax({
+					url: 'index.php?option=com_content&task=article.shareDraft&format=json',
+					data: share_draft,
+					dataType: "json"
+				})
+				.done(function (response) {
+					// Render messages, if any.
+					if (typeof response.messages == 'object' && response.messages !== null) {
+						Joomla.renderMessages(response.messages);
+						window.scrollTo(0, 0);
+					}
+				})
+				.fail(function (xhr, ajaxOptions, thrownError) {
+					Joomla.renderMessages(xhr.responseText);
+					window.scrollTo(0, 0);
+				});
+			} else {
+				joomla.submitform(task);
+			}
+		};
+	});
+})(jQuery);
