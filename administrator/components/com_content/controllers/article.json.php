6d09c54b0ca2270c908ab6135820c3901e627853
Nikita Dhiman <nikitadhmn@gmail.com>
2016-08-17 10:04:41 +0530

    minor

--- administrator/components/com_content/controllers/article.json.php
+++ administrator/components/com_content/controllers/article.json.php
@@ -17,40 +17,40 @@
  */
  class ContentControllerArticle extends JControllerForm
 {
-	    /**
-	     * Method to generate and store share token.
-	     *
-	     * @return  boolean   True if token successfully stored, false otherwise and internal error is set.
-	     *
-	     * @since   3.7
-	     */
-	     public function shareDraft()
-	     {
-	      	      $app    = JFactory::getApplication();
+		/**
+		 * Method to generate and store share token.
+		 *
+		 * @return  boolean   True if token successfully stored, false otherwise and internal error is set.
+		 *
+		 * @since   3.7
+		 */
+		 public function shareDraft()
+		 {
+				  $app    = JFactory::getApplication();
 		
-	    	      if (!JSession::checkToken())
-	    	      {
-	      		        echo new JResponseJson(null, JText::_('JINVALID_TOKEN'), true);
-	      		        $app->close();
-		          }
+				  if (!JSession::checkToken('get'))
+				  {
+					echo new JResponseJson(null, JText::_('JINVALID_TOKEN'), true);
+					$app->close();
+				  }
 		
-	    	      $return = false;
-	    	      $error = false;
-	    	      $message = JText::_('COM_CONTENT_TOKEN_SAVED');
+				  $return = false;
+				  $error = false;
+				  $message = JText::_('COM_CONTENT_TOKEN_SAVED');
 		
-	    	      try
-		          {
-			               // Get the model
-			              $model = $this->getModel();
-			              $return = $model->shareToken();
-		          }
-		          catch (Exception $e)
-		          {
-			              $error = true;
-			              $message = JText::_('COM_CONTENT_TOKEN_ERROR');
-		          }
+				  try
+				  {
+					// Get the model
+					$model = $this->getModel();
+					$return = $model->shareToken();
+				  }
+				  catch (Exception $e)
+				  {
+						  $error = true;
+						  $message = JText::_('COM_CONTENT_TOKEN_ERROR');
+				  }
 		
-		          echo new JResponseJson($return, $message, $error);
-		          $app->close();
-	     }
+				  echo new JResponseJson($return, $message, $error);
+				  $app->close();
+		 }
  }
