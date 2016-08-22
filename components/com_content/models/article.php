afd624ef0d17aaab5637501ee4d3f57a168d48c7
Nikita Dhiman <nikitadhmn@gmail.com>
2016-08-22 10:57:31 +0530

    minor

--- components/com_content/models/article.php
+++ components/com_content/models/article.php
@@ -179,7 +179,7 @@ class ContentModelArticle extends JModelItem
 				// Check for published state if filter set.
 				if (((is_numeric($published)) || (is_numeric($archived))) && (($data->state != $published) && ($data->state != $archived)))
 				{
-					if( !($haveToken == $currentToken))
+					if ( !($haveToken == $currentToken))
 					{
 						
 						return JError::raiseError(404, JText::_('COM_CONTENT_ERROR_ARTICLE_NOT_FOUND'));
